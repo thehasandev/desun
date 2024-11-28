541e6c085fd7b1eb8c99db1b7665f8a8e264b8ce
Hasan <thehasandev@gmail.com>
2024-11-28 22:39:01 +0600

    fonts setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+export const microsoftYahei = localFont({
+  src: "./fonts/MicrosoftYaheifont.otf",
+  variable: "--font-microsoft-yahei",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${microsoftYahei.className} antialiased`}>
         {children}
       </body>
     </html>
